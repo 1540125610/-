957485107cdba3e94dac80037bc72749202aa035
王俊杰 <1540125610@qq.com>
2021-03-09 16:19:02 +0800

    导航网格初步实现

---
+++ Cscript/GridScript.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridScript : MonoBehaviour
+{
+    public bool isAppropriat;           //是否被占用
+    public GridScript[] grids;            //周围脚本
+    public GridsControl gridsControl;               //格子汇总信息
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isAppropriat = false;           //初始未被占用
+
+        grids = new GridScript[10];     //周围脚本(0不使用，1为左下，9为右上)
+
+        gridsControl = GameObject.Find("Grids Control").GetComponent<GridsControl>();
+        GetOtherGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void GetOtherGrid()
+    {
+        float posX = transform.position.x;
+        float posY = transform.position.y;
+        float posZ = transform.position.z;
+        foreach (GridScript a in gridsControl.gridScripts)
+        {
+            Vector3 aPos = a.transform.position;
+
+            if(Mathf.Abs(aPos.x-posX)>2 || Mathf.Abs(aPos.z - posZ) > 2)    //不是周围的直接跳过
+            {
+                continue;
+            }
+
+
+            //左下 1
+            if(aPos.x == posX-1*2 && aPos.y == posY && aPos.z ==posZ-1*2)
+            {
+                grids[1] = a;
+            }
+            //下  2
+            else if (aPos.x == posX && aPos.y == posY && aPos.z == posZ - 1*2)
+            {
+                grids[2] = a;
+            }
+            //右下 3
+            else if (aPos.x == posX + 1*2 && aPos.y == posY && aPos.z == posZ - 1*2)
+            {
+                grids[3] = a;
+            }
+            //左  4
+            else if (aPos.x == posX - 1*2 && aPos.y == posY && aPos.z == posZ)
+            {
+                grids[4] = a;
+            }
+            //自己 5
+            else if (aPos.x == posX && aPos.y == posY && aPos.z == posZ)
+            {
+                grids[5] = a;
+            }
+            //右  6
+            else if (aPos.x == posX + 1*2 && aPos.y == posY && aPos.z == posZ)
+            {
+                grids[6] = a;
+            }
+            //左上 7
+            else if (aPos.x == posX - 1*2 && aPos.y == posY && aPos.z == posZ + 1*2)
+            {
+                grids[7] = a;
+            }
+            //上 8
+            else if (aPos.x == posX && aPos.y == posY && aPos.z == posZ + 1*2)
+            {
+                grids[8] = a;
+            }
+            //右上 9
+            else if (aPos.x == posX + 1*2 && aPos.y == posY && aPos.z == posZ + 1*2)
+            {
+                grids[9] = a;
+            }
+
+        }
+    }
+
+    public void OnAppropriat()
+    {
+        isAppropriat = true;
+    }
+    public void OffOnAppropriat()
+    {
+        isAppropriat = false;
+    }
+}
---
+++ Cscript/GridsControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridsControl : MonoBehaviour
+{
+    public GameObject grid;             //网格实体
+    public GameObject Grids;            //网格汇总的父物体
+    public GridScript[] gridScripts;    //所有网格的脚本
+
+    private void Awake()
+    {
+        gridScripts = new GridScript[2500];     //2500个格子
+
+        int x, y;                               //x：行，y列 左下为原点
+        for (int i = 0; i < 2500; i++)
+        {
+            x = i%50;   
+            y = i/50;
+            Vector3 pos = new Vector3(-49 + x*2, -1, -49 + y*2);  //从左下开始创建 格子大小为2*2
+
+            GameObject newGrid = GameObject.Instantiate(grid, pos, Quaternion.identity);
+
+            newGrid.name = "Grid" + y +"-"+ x;      //名字
+            newGrid.transform.parent = Grids.transform;     //移动到总网格下
+                
+            gridScripts[i] = newGrid.GetComponent<GridScript>();        //将其脚本列入脚本汇总
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+}
